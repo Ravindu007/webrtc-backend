a5f655211a29a5aa4b3d154ff0944ff036926ad6
Ravindu007 <ravindu0504@gmail.com>
2024-03-25 08:48:25 +0530

    changed cors

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,33 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware'; // Import proxy middleware
-
 import { RoomHandler } from './room/intex';
 
 const port = 8080;
 const app = express();
-
-// Enable CORS for all routes
-app.use(cors());
-
-// Proxy requests to the WebSocket server while including CORS headers
-app.use('/socket.io', createProxyMiddleware({
-    target: 'https://webrtc-backend-ex84.onrender.com',
-    changeOrigin: true,
-    ws: true,
-    pathRewrite: {
-        '^/socket.io': '/socket.io'
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    }
+app.use(cors({
+    origin: '*'
 }));
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-requested-Width, Content-Type, Accept"
+    )
+    next()
+})
 
 const server = http.createServer(app);
 
 // socket instance (socket server)
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on("connection", (socket) => {
     console.log('user is connected');
