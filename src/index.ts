df125d9d8ab83e0c728a1b477f3df35bc97af1f5
Ravindu007 <ravindu0504@gmail.com>
2024-03-25 08:14:45 +0530

    changed proxy issue

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,34 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware'; // Import proxy middleware
+
 import { RoomHandler } from './room/intex';
 
 const port = 8080;
 const app = express();
-app.use(cors);
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Proxy requests to the WebSocket server while including CORS headers
+app.use('/socket.io', createProxyMiddleware({
+    target: 'https://webrtc-backend-ex84.onrender.com',
+    changeOrigin: true,
+    ws: true,
+    pathRewrite: {
+        '^/socket.io': '/socket.io'
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    }
+}));
 
 const server = http.createServer(app);
 
 // socket instance (socket server)
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 
 io.on("connection", (socket) => {
     console.log('user is connected');
