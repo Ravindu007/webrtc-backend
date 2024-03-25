4b2d2417da22b2878027ebcdc6b4c736811a29d1
Ravindu007 <ravindu0504@gmail.com>
2024-03-25 09:00:54 +0530

    chanegd cors

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,8 @@ import { RoomHandler } from './room/intex';
 
 const port = 8080;
 const app = express();
-app.use(cors({
-    origin: '*'
-}));
 
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-requested-Width, Content-Type, Accept"
-    )
-    next()
-})
+app.use(cors()); // Use CORS middleware for all routes
 
 const server = http.createServer(app);
 
